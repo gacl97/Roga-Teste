ac582513123f5404fa50e78e1ca2784a2df4d982
Gustavo Augusto <gacl@ic.ufal.br>
2020-08-27 17:51:05 -0300

    Config of Errors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata'; // Use decorator syntax
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import AppError from '../../errors/AppError';
 
 import routes from './routes';
 
@@ -8,6 +10,25 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+// Middleware for generating errors
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Servidor rodando na porta: 3333');
